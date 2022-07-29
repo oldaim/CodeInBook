67105e5160a3b311c317b11fb018e121933598f2
Kimdongkyun <dgk0911@gmail.com>
2022-07-29 11:49:50 +0900

    chapter5 예제

---
+++ src/javaInAction/chapter5/practice/Trader.java
@@ -0,0 +1,24 @@
+package javaInAction.chapter5.practice;
+
+public class Trader {
+    private final String name;
+    private final String city;
+
+    public Trader(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getCity(){
+        return this.city;
+    }
+
+    @Override
+    public String toString(){
+        return "Trader:"+this.name+"in"+this.city;
+    }
+}
